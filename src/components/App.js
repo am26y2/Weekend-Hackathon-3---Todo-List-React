c8500aa5b1a3d650df59b5b8e1f9b4766f6befa7
Amit Raj <amitraj9046@gmail.com>
2020-10-31 17:18:25 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [listItem, setListItem] = useState([]);
+  const [input, setinput] = useState("");
 
+  const ToDoList = (props) => {
+    const { item, onDelete, id } = props;
+    if (item === "") return null;
+    return (
+      <>
+        <div className="row">
+          <li>{item}</li>
+          <button onClick={() => onDelete(id)}>
+            <span>❌</span>
+          </button>
+        </div>
+      </>
+    );
+  };
+  const handleDelete = (id) => {
+    const filtervalue = listItem.filter((item, index) => index !== id);
+    setListItem(filtervalue);
+    console.log(id);
+  };
+  const handleChange = (event) => {
+    setinput(event.target.value);
+    console.log("clicked");
+  };
+
+  const additem = () => {
+    const addItem = [...listItem, input];
+    setListItem(addItem);
+    setinput("");
+  };
+  return (
+    <div id="main">
+      <div className="header">
+        <h1>ToDo-List</h1>
+      </div>
+      <div>
+        <input
+          id="task"
+          type="input"
+          placeholder="AddItem"
+          value={input}
+          onChange={handleChange}
+        />
+        <button id="btn" onClick={additem}>
+          <span>➕</span>
+        </button>
+      </div>
+      <br />
+      <div className="list">
+        <ol>
+          {listItem.map((itemval, index) => {
+            return (
+              <ToDoList
+                item={itemval}
+                key={index}
+                id={index}
+                onDelete={handleDelete}
+              />
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
 
 export default App;
